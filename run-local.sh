58bd406834ce3b687272927dfef3730c0336df72
Michio Nakagawa <michio.nakagawa@gmail.com>
2014-01-19 02:26:53 +0900

    fix: cdh

--- run-local.sh
+++ run-local.sh
@@ -4,7 +4,6 @@ rm -rf output
 
 echo "Hello World Bye World" > input/file0
 echo "Hello Hadoop Goodbye Hadoop" > input/file1
-hadoop fs -put input/file0 input/
-hadoop fs -put input/file1 input/
 
 hadoop jar ./target/wordcount-1.0.jar org.myorg.WordCount input/ output/
+
